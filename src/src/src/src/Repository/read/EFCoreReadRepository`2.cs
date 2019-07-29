d2a4573e7d8b7df4b9a5f134c6fa0992a94bc3d9
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2019-07-29 16:22:43 +0300

    Implemented primary key generic type to read repository

--- src/src/src/src/Repository/read/EFCoreReadRepository`2.cs
+++ src/src/src/src/Repository/read/EFCoreReadRepository`2.cs
@@ -1,5 +1,5 @@
 ﻿//-----------------------------------------------------------------------
-// <copyright file="EFCoreReadRepository`1.cs" company="Bayhaksam">
+// <copyright file="EFCoreReadRepository`2.cs" company="Bayhaksam">
 //      Copyright (c) Bayhaksam. All rights reserved.
 // </copyright>
 // <author>Samet Kurumahmut</author>
@@ -14,7 +14,8 @@ namespace Bayhaksam.EFCore.Repository
 	using System.Linq;
 	using System.Linq.Expressions;
 
-	public class EFCoreReadRepository<TEntity> : EFCoreRepositoryBase, IReadRepository<TEntity> where TEntity : class
+	public class EFCoreReadRepository<TEntity, TPrimaryKey>
+		: EFCoreRepositoryBase, IReadRepository<TEntity, TPrimaryKey> where TEntity : class
 	{
 		#region Constructors
 		public EFCoreReadRepository(DbContext context) : base(context)
@@ -24,17 +25,17 @@ namespace Bayhaksam.EFCore.Repository
 		#endregion
 
 		#region IReadRepository Methods
-		public virtual bool IsExists(int id) => this.Context.Set<TEntity>().Find(id) != null;
+		public virtual bool IsExists(TPrimaryKey id) => this.Context.Set<TEntity>().Find(id) != null;
 
 		public virtual IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate) =>
 			this.Context.Set<TEntity>().Where(predicate);
 
 		public virtual IEnumerable<TEntity> GetAll() => this.Context.Set<TEntity>().ToList();
 
-		public virtual TEntity Get(int id) => this.Context.Set<TEntity>().Find(id);
+		public virtual TEntity FirstOrDefault(Expression<Func<TEntity, bool>> predicate) =>
+			this.Context.Set<TEntity>().FirstOrDefault(predicate);
 
-		public virtual TEntity SingleOrDefault(Expression<Func<TEntity, bool>> predicate) =>
-			this.Context.Set<TEntity>().SingleOrDefault(predicate);
+		public virtual TEntity Get(TPrimaryKey id) => this.Context.Set<TEntity>().Find(id);
 		#endregion
 	}
 }
